f27dcdb239de09b348da0bdeb4f10b8171487fe4
The Tiến <tiendt2311@gmail.com>
2024-05-27 12:49:21 +1000

    chore(order): add order by discount

--- src/components/QueryActions/index.tsx
+++ src/components/QueryActions/index.tsx
@@ -1,17 +1,58 @@
-import { ArrowPathIcon, Bars3BottomRightIcon, MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline";
+import { Bars3BottomRightIcon, MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline";
 import {
   Button, Input, Menu, MenuHandler, MenuItem, MenuList, Typography
 } from "@material-tailwind/react";
 import { QueryProps } from "../../types";
 import QueryDetails from "./QueryDetails";
+import { ArrowUpIcon, CalendarDaysIcon, CurrencyDollarIcon, ArrowDownIcon, ReceiptPercentIcon } from "@heroicons/react/24/solid";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
-const SortButton = (
-  { handleSort }: { handleSort: (sort: { [key: string]: string }) => void; }
-) => (
+type OrderProps = {
+  created_at?: string;
+  price?: string;
+  discount?: string;
+}
+const SortIcon = ({ order }: { order?: OrderProps }) => {
+  if (!order) return <Bars3BottomRightIcon className="h-5 w-5" />;
+
+  if (order.created_at) {
+    return (
+      <span className="flex">
+        { order.created_at === 'desc' ? <ArrowDownIcon className="h-5 w-5" /> : <ArrowUpIcon className="h-5 w-5" /> }
+        <CalendarDaysIcon className="h-5 w-5" />
+      </span>
+    )
+  }
+
+  if (order.price) {
+    return (
+      <span className="flex">
+        { order.price === 'desc' ? <ArrowDownIcon className="h-5 w-5" /> : <ArrowUpIcon className="h-5 w-5" /> }
+        <CurrencyDollarIcon className="h-5 w-5" />
+      </span>
+    )
+  }
+
+  if (order.discount) {
+    return (
+      <span className="flex">
+        { order.discount === 'desc' ? <ArrowDownIcon className="h-5 w-5" /> : <ArrowUpIcon className="h-5 w-5" /> }
+        <ReceiptPercentIcon className="h-5 w-5" />
+      </span>
+    )
+  }
+}
+
+type SortButtonProps = {
+  handleSort: (sort: { [key: string]: string }) => void;
+  currentOrder?: { [key: string]: string };
+}
+
+const SortButton = ( { handleSort, currentOrder }: SortButtonProps) => (
   <Menu>
     <MenuHandler>
       <Button>
-        <Bars3BottomRightIcon className="h-5 w-5" />
+        <SortIcon order={currentOrder} />
       </Button>
     </MenuHandler>
     <MenuList>
@@ -26,6 +67,9 @@ const SortButton = (
       </MenuItem>
       <MenuItem onClick={() => handleSort({ price: 'desc' })} key='price-desc'>
         Price highest to lowest
+      </MenuItem>
+      <MenuItem onClick={() => handleSort({ discount: 'desc' })} key='discount-desc'>
+        Discount highest to lowest
       </MenuItem>
     </MenuList>
   </Menu>
@@ -58,6 +102,10 @@ const SearchInput = ({ queryName, handleQueryNameChange }: SearchInputProps) => 
   </div>
 );
 
+const isQueryEmpty = (query: QueryProps) => {
+  return Object.keys(query).every(key => !query[key as keyof QueryProps]);
+}
+
 type QueryActionsProps = {
   queryName: string;
   handleQueryNameChange: (value: string) => void;
@@ -77,10 +125,12 @@ const QueryActions = ({
   <div>
     <div className="flex">
       <SearchInput queryName={queryName} handleQueryNameChange={handleQueryNameChange} />
-      <SortButton handleSort={handleSort} />
-      <Button className="ml-2" onClick={handleResetQuery}>
-        <ArrowPathIcon className="h-5 w-5" />
-      </Button>
+      <SortButton handleSort={handleSort} currentOrder={query.order}/>
+      { !isQueryEmpty(query) && (
+        <Button className="ml-2" onClick={handleResetQuery}>
+          <XMarkIcon className="h-5 w-5" />
+        </Button>
+      )}
     </div>
     <QueryDetails query={query} handleQuery={handleQuery} />
   </div>
